690072df96308b292005df20aafbb0e47243314d
marcelooliveira <mclricardo@gmail.com>
2016-12-16 17:40:48 -0200

    Aula 6

---
+++ TestDrive/TestDrive.Aula6/Models/ListagemVeiculos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrive.Models
+{
+    public class ListagemVeiculos
+    {
+        public List<Veiculo> Veiculos { get; set; }
+
+        public ListagemVeiculos()
+        {
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo { Nome = "Azera V6", Preco = 60000 },
+                new Veiculo { Nome = "Fiesta 2.0", Preco = 50000 },
+                new Veiculo { Nome= "HB20 S", Preco = 40000 }
+            };
+        }
+    }
+}
