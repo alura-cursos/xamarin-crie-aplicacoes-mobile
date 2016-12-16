e9a5c70fc26ee8ba9e9182e5a059bbbffd2d3f9e
marcelooliveira <mclricardo@gmail.com>
2016-12-16 17:50:19 -0200

    Aula 7

---
+++ TestDrive/TestDrive.Aula7/ViewModels/ListagemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Models;
+using Xamarin.Forms;
+
+namespace TestDrive.ViewModels
+{
+    public class ListagemViewModel
+    {
+        public List<Veiculo> Veiculos { get; set; }
+
+        Veiculo veiculoSelecionado;
+        public Veiculo VeiculoSelecionado
+        {
+            get
+            {
+                return veiculoSelecionado;
+            }
+            set
+            {
+                veiculoSelecionado = value;
+                if (value != null)
+                    MessagingCenter.Send(veiculoSelecionado, "VeiculoSelecionado");
+            }
+        }
+
+        public ListagemViewModel()
+        {
+            this.Veiculos = new ListagemVeiculos().Veiculos;
+        }
+    }
+}
