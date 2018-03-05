a662c4b0eddddba12d37d9e2226293ca41e2d1f6
marcelooliveira <mclricardo@gmail.com>
2018-03-05 15:58:21 -0300

    Aula6

---
+++ TestDrive/TestDrive.Aula6/ViewModels/ListagemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Models;
+
+namespace TestDrive.ViewModels
+{
+    public class ListagemViewModel
+    {
+        public List<Veiculo> Veiculos { get; set; }
+
+        public ListagemViewModel()
+        {
+            this.Veiculos = new ListagemVeiculos().Veiculos;
+        }
+    }
+}
