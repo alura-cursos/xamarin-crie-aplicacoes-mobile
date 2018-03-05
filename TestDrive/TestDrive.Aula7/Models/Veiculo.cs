da81412a502c13d85da2c400d4dea0e5fb0b864f
marcelooliveira <mclricardo@gmail.com>
2018-03-05 16:58:20 -0300

    Aula7

---
+++ TestDrive/TestDrive.Aula7/Models/Veiculo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrive.Models
+{
+    public class Veiculo
+    {
+        public const int FREIO_ABS = 800;
+        public const int AR_CONDICIONADO = 1000;
+        public const int MP3_PLAYER = 500;
+
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R$ {0}", Preco); }
+        }
+
+        public bool TemFreioABS { get; set; }
+        public bool TemArCondicionado { get; set; }
+        public bool TemMP3Player { get; set; }
+
+        public string PrecoTotalFormatado
+        {
+            get
+            {
+                return string.Format("Valor Total: R$ {0}",
+                    Preco
+                    + (TemFreioABS ? FREIO_ABS : 0)
+                    + (TemArCondicionado ? AR_CONDICIONADO : 0)
+                    + (TemMP3Player ? MP3_PLAYER : 0)
+                    );
+            }
+        }
+    }
+}
---
+++ TestDrive/TestDrive.Aula7/Views/ListagemView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Models;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class ListagemView : ContentPage
+    {
+        public ListagemView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<Veiculo>(this, "VeiculoSelecionado",
+                (msg) =>
+                {
+                    Navigation.PushAsync(new DetalheView(msg));
+                });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            MessagingCenter.Unsubscribe<Veiculo>(this, "VeiculoSelecionado");
+        }
+    }
+}
