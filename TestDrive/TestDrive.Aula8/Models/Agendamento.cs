4fc4234fb3c51d4fe15ba8931020f15294a1d3f9
marcelooliveira <mclricardo@gmail.com>
2018-03-05 17:08:57 -0300

    Aula8

---
+++ TestDrive/TestDrive.Aula8/Models/Agendamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrive.Models
+{
+    public class Agendamento
+    {
+        public Veiculo Veiculo { get; set; }
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Email { get; set; }
+
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento
+        {
+            get
+            {
+                return dataAgendamento;
+            }
+            set
+            {
+                dataAgendamento = value;
+            }
+        }
+
+        public TimeSpan HoraAgendamento { get; set; }
+    }
+}
---
+++ TestDrive/TestDrive.Aula8/Views/AgendamentoView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Models;
+using TestDrive.ViewModels;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class AgendamentoView : ContentPage
+    {
+        public AgendamentoViewModel ViewModel { get; set; }
+
+        public AgendamentoView(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.ViewModel = new AgendamentoViewModel(veiculo);
+            this.BindingContext = this.ViewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<Agendamento>(this, "Agendamento",
+                async (msg) =>
+                {
+                    var confirma = await DisplayAlert("Salvar Agendamento",
+                    "Deseja mesmo enviar o agendamento?",
+                    "sim", "não");
+
+                    if (confirma)
+                    {
+                        this.ViewModel.SalvarAgendamento();
+                    }
+                });
+
+            MessagingCenter.Subscribe<Agendamento>(this, "SucessoAgendamento",
+                (msg) =>
+                {
+                    DisplayAlert("Agendamento", "Agendamento salvo com sucesso!", "ok");
+                });
+
+            MessagingCenter.Subscribe<ArgumentException>(this, "FalhaAgendamento",
+                (msg) =>
+                {
+                    DisplayAlert("Agendamento", "Falha ao agendar o test drive! Verifique os dados e tente novamente mais tarde!", "ok");
+                });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<Agendamento>(this, "Agendamento");
+
+            MessagingCenter.Unsubscribe<Agendamento>(this, "SucessoAgendamento");
+            MessagingCenter.Unsubscribe<ArgumentException>(this, "FalhaAgendamento");
+        }
+    }
+}
